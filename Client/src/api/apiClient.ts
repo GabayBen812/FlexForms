de7e6a3ac62aa395e462955af996c0dca9b7a11f
Ben <7ben7ben@gmail.com>
2025-05-18 20:02:29 +0300

    test this

--- Client/src/api/apiClient.ts
+++ Client/src/api/apiClient.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+// const API_BASE_URL=http://localhost:3101 # For Development
+const API_BASE_URL = "https://flexforms-production.up.railway.app";
 
 if (!API_BASE_URL) {
   console.warn('VITE_API_BASE_URL is not set in environment variables');
 }
+
+console.log('API_BASE_URL:', API_BASE_URL);
+
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,

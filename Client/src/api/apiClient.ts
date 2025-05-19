66d60dd4553bbb7b1b817ad3eece3a130d60b616
Ben <7ben7ben@gmail.com>
2025-05-19 18:08:02 +0300

    test

--- Client/src/api/apiClient.ts
+++ Client/src/api/apiClient.ts
@@ -9,6 +9,8 @@ if (!API_BASE_URL) {
     : "https://flexforms-production.up.railway.app";
 }
 
+console.log("VITE_API_BASE_URL:", import.meta.env.VITE_API_BASE_URL);
+console.log("MODE:", import.meta.env.MODE);
 console.log("API_BASE_URL:", API_BASE_URL);
 
 const apiClient = axios.create({

0688c1688d4e23c6e5b1e58fa25a0b803f0d0820
bengabay812 <7ben7ben@gmail.com>
2025-05-29 14:13:27 +0300

    fix bug

--- Client/src/hooks/use-toast.ts
+++ Client/src/hooks/use-toast.ts
@@ -3,7 +3,7 @@
 // Inspired by react-hot-toast library
 import * as React from "react";
 
-import type { ToastActionElement, ToastProps } from "@/components/ui/toast";
+import type { ToastProps } from "@/components/ui/toast";
 
 const TOAST_LIMIT = 1;
 const TOAST_REMOVE_DELAY = 100000;
@@ -12,7 +12,7 @@ type ToasterToast = ToastProps & {
   id: string;
   title?: React.ReactNode;
   description?: React.ReactNode;
-  action?: ToastActionElement;
+  action?: any;
 };
 
 const actionTypes = {
